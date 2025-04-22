3c3aec0f2323379cd6b4d626145f64cab1de75d8
Joe <joeoffutt4@gmail.com>
2025-04-21 21:18:47 -0400

    Update Example3-Answer.tsx

--- src/components/Example3-Answer.tsx
+++ src/components/Example3-Answer.tsx
@@ -4,6 +4,9 @@ export function Example3Answer() {
   const [localVariable1, setLocalVariable1] = useState(0);
   const [localVariable2, setLocalVariable2] = useState(localVariable1 + 1);
 
+  //could also do this (if it is a strict requirement its always 1 higher)
+  //const localVariable2 = useMemo(() => localVariable1+1,[localVariable1]);
+
   return (
     <div>
       <button
